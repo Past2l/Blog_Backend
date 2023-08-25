ba73d547690c98408cb2889ba73894f9ec94f7c4
Dohyeon Yun <to@dohyun.it>
2023-08-26 00:40:09 +0900

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      ...(process.env.NODE_ENV == 'production' && {
+        disableErrorMessages: true,
+      }),
+    }),
+  );
+  if (process.env.NODE_ENV != 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Yun Dohyun Blog Backend API Document')
+      .setDescription('blog.dohyun.it')
+      .setVersion('0.0.1')
+      .addCookieAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
+  app.enableCors();
+  app.use(cookieParser());
+
+  await app.listen(parseInt(process.env.SERVER_PORT) || 3000);
+}
+bootstrap();
